ecf184d60e16147c03739602e480a2e582b47726
919701 <fisapser@gmail.com>
2023-03-16 23:41:36 +0500

    feat: Task 4

--- resources/sql/sql-task.sql
+++ resources/sql/sql-task.sql
@@ -1,11 +1,15 @@
 --Вывести к каждому самолету класс обслуживания и количество мест этого класса
-select aircraft_code, fare_conditions, count(seat_no) total_seats
+select aircraft_code,
+       fare_conditions,
+       count(seat_no) total_seats
 from seats
-group by aircraft_code, fare_conditions
-order by aircraft_code, fare_conditions;
+group by aircraft_code,
+         fare_conditions
+order by aircraft_code,
+         fare_conditions;
 
 --Найти 3 самых вместительных самолета (модель + кол-во мест)
-select (select model ->> 'en'
+select (select model ->> 'en' aircraft_model
         from aircrafts_data
         where seats.aircraft_code = aircrafts_data.aircraft_code),
        count(seat_no) total_seats
@@ -15,27 +19,34 @@ order by total_seats desc
 limit 3;
 
 --Вывести код,модель самолета и места не эконом класса для самолета 'Аэробус A321-200' с сортировкой по местам
-select seats.aircraft_code,
-       seat_no,
-       fare_conditions,
-       aircrafts_data.model ->> 'ru' model_aircraft
-from seats,
-     aircrafts_data
-where fare_conditions != 'Economy'
-  and model ->> 'ru' = 'Аэробус A321-200'
+select distinct aircrafts_data.aircraft_code,
+                model ->> 'ru' aircraft_model,
+                seat_no,
+                fare_conditions
+from aircrafts_data,
+     seats
+where model ->> 'ru' = 'Аэробус A321-200'
+  and fare_conditions <> 'Economy'
 order by seat_no;
 
 --Вывести города в которых больше 1 аэропорта ( код аэропорта, аэропорт, город)
-with one_city as (select city ->> 'en' city
-                  from airports_data
-                  group by city
-                  having count(*) > 1)
-select airport_code,airport_name->>'en' airport_name,city->>'en'
+select airport_code,
+       airport_name ->> 'en' airport_name,
+       city ->> 'en'
 from airports_data
-where city ->> 'en' in (select city from one_city)
-order by city->>'en';
+where city ->> 'en' in (select city ->> 'en'
+                        from airports_data
+                        group by city ->> 'en'
+                        having count(*) > 1)
+order by city ->> 'en';
 
 -- Найти ближайший вылетающий рейс из Екатеринбурга в Москву, на который еще не завершилась регистрация
+select *
+from flights_v
+where status in ('On Time','Departed')
+  and departure_city = 'Екатеринбург'
+  and arrival_city = 'Москва'
+limit 1;
 
 --Вывести самый дешевый и дорогой билет и стоимость ( в одном результирующем ответе)
 
